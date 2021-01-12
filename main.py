c301a8f1d2d36d2d943c3752e5fec4dfd4bcddd3
AhmadBilal07 <ahmadbilal07@live.com>
2021-01-13 01:14:03 +0500

    Updated Bounding Box Issue

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 #Importing Necessary Libraries
 from PIL import Image,ImageDraw
 import numpy as np
-from collections import OrderedDict
+from collections import OrderedDict,defaultdict
 
 
 #Path of the Image whose components are to be found
-IMG_PATH = "sample images/shapes4.png"
+IMG_PATH = "sample images/shapes.png"
 
 #Opening the img and converting it into Black and white
 img = Image.open(IMG_PATH)
 thresh = 220
 fn = lambda x : 255 if x > thresh else 0
 img = img.convert('L').point(fn, mode='1')
-
-
-
-img.show()
 
 
 #Converting img into array
@@ -46,10 +42,10 @@ for i in range(num_rows):
             if data[i - 1][j] != 1 and data[i][j - 1] != 1:
                 if data[i - 1][j] < data[i][j - 1]:
                     data[i][j] = data[i - 1][j]
-                    conflictedLabels[data[i-1][j]] = data[i][j - 1]
+                    conflictedLabels[data[i-1][j]] = (data[i][j - 1])
                 elif data[i][j - 1] < data[i - 1][j]:
                     data[i][j] = data[i][j - 1]
-                    conflictedLabels[data[i][j-1]] =  data[i-1][j ]
+                    conflictedLabels[data[i][j-1]] = (data[i-1][j ])
                 else:
                     data[i][j] = data[i-1][j]
 
@@ -58,9 +54,10 @@ for i in range(num_rows):
             if data[i - 1][j] == 1 and data[i][j - 1] != 1:
                 data[i][j] = data[i][j - 1]
 
-
 #Sorting Dict in descending order
 OrderedDict(sorted(conflictedLabels.items(), key=lambda t: t[0]))
+
+
 
 #Re-Labelling
 for key in reversed(list(conflictedLabels.keys())):
@@ -75,21 +72,44 @@ componentsCount = totalComponents.__len__() - 1
 print("Total Components: ", componentsCount)
 
 '''
-ComponentsCoordinate:
-[componentNumber,ComponentColourValue,x1,y1,x2,y2]
+ComponentsList:
+[componentNumber,x1,y1,x2,y2]
 '''
-
 componentsList = []
+
+
+
 #Finding Coordinates of Connected Components
 for index,i in enumerate(totalComponents[1:]):
-    arr = np.where(data == i)
-    arr_Length_Xaxis = arr[0].__len__()
-    arr_Length_Yaxis = arr[1].__len__()
-    componentsList.append([index+1,i,arr[0][0],arr[1][0],arr[0][arr_Length_Xaxis-1],arr[1][arr_Length_Yaxis-1]])
+    flag=0
+    x1 = -1
+    x2 = -1
+    y1 = -1
+    y2 = -1
+    for j in range (num_rows):
+        for k in range(num_columns):
+            if data[j][k] == i:
+                flag+=1
+                if(flag == 1):
+                    y1=j
+                    x1 =k
+                if(k < x1):
+                    x1 = k
+                if (k > x2):
+                    x2 = k;
+                y2 = j
+    componentsList.append([index+1,x1,y1,x2,y2])
+
+
+# Colouring Components
+for index,i in enumerate(totalComponents[1:]):
+    data[data == i] = (index+1)*25
+img = Image.fromarray(data)
+img.show()
 
 print("Components List : ",componentsList)
-img = Image.fromarray(data)
-#img.show()
+
+
 
 
 # Opening Original Image
@@ -98,6 +118,6 @@ img = Image.open(IMG_PATH)
 #Drawing Bounding Box over the connected components:
 for i in componentsList:
     draw = ImageDraw.Draw(img)
-    draw.rectangle([i[3],i[2],i[5],i[4]],outline="#FFFF00")
+    draw.rectangle([i[1],i[2],i[3],i[4]],outline="#FFFF00")
 
 img.show()
